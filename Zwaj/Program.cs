c3ff3296ab986a9b841b8fcab9d22ae4137d5bf3
Mohamed Blgikey <MohamedAbdellatif@forttech.com>
2022-02-17 18:56:45 +0200

    handel error

--- Zwaj/Program.cs
+++ Zwaj/Program.cs
@@ -1,7 +1,9 @@
 using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Diagnostics;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.IdentityModel.Tokens;
+using System.Net;
 using System.Text;
 using Zwaj.BL.Helper;
 using Zwaj.BL.Interfaces;
@@ -57,6 +59,22 @@ if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
     app.UseSwaggerUI();
+}
+else
+{
+    app.UseExceptionHandler(b =>
+    {
+        b.Run(async context =>
+        {
+            context.Response.StatusCode = (int)HttpStatusCode.InternalServerError;
+            var error = context.Features.Get<IExceptionHandlerFeature>();
+            if (error != null)
+            {
+                context.Response.AddApplicationError(error.Error.Message);
+                await context.Response.WriteAsync(error.Error.Message);
+            }
+        });
+    });
 }
 
 app.UseCors("CorsPolicy");
